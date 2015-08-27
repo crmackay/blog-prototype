b036256d371cbcedef8f787ae204ef4ed44d53ca
Chris <crmackay@gmail.com>
2015-08-26 21:40:50 -0400

    adding rotation of menu logo

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
-function showMenu(){
+function toggleMenu(){
     document.getElementById("sidebar").classList.toggle("visible");
+    document.getElementById("menuBtn").classList.toggle("rotate");
 }
 
-document.getElementById("menuBtn").addEventListener("click", showMenu);
+document.getElementById("menuBtn").addEventListener("click", toggleMenu);
 
