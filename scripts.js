e4d829d851b8909b6ed07fbf117494f236295f91
Chris <crmackay@gmail.com>
2015-08-27 00:01:24 -0400

    add slide out functionality

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 function toggleMenu(){
-    document.getElementById("sidebar").classList.toggle("visible");
+    document.getElementById("container").classList.toggle("slide");
+    document.getElementById("sidebar").classList.toggle("slide");
+//    document.getElementById("menuBtn").classList.toggle("slide, rotate");
     document.getElementById("menuBtn").classList.toggle("rotate");
+    document.getElementById("menuBtn").classList.toggle("slide");
 }
 
 document.getElementById("menuBtn").addEventListener("click", toggleMenu);
